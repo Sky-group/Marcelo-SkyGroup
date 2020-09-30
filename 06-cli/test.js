8a5ee4f421e0d46da5905a40cfb3dd8bd455e5f5
marcelocardosodev <cardosom2003dev@gmail.com>
2020-09-30 00:44:51 -0300

    Manipulando arquivos - UPDATE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -8,10 +8,16 @@ const DEFULT_ITEM_CADASTRADO = {
     id: 1
 }
 
+const DEFULT_ITEM_ATUALIZAR = {
+    nome:  'Lanterna Verde',
+    poder: 'Energia do Anel',
+    id: 2
+}
 describe('Suite de manipulação de heroi', ()=>{
 
     before(async ()=>{
         await database.cadastrar(DEFULT_ITEM_CADASTRADO);
+        await database.cadastrar(DEFULT_ITEM_ATUALIZAR);
     });
 
     it('Deve pesquisaar um heroi, utilizando arquivos', async ()=>{
@@ -32,6 +38,23 @@ describe('Suite de manipulação de heroi', ()=>{
         const expected = true; 
         const resultado = await database.remover(DEFULT_ITEM_CADASTRADO.id);
         deepEqual(resultado, expected)
+    })
+
+    it('Deve atualizar um heroi pelo id', async ()=>{
+        const expected = {
+            ...DEFULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        const novoDado = {
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        await database.atualizar(DEFULT_ITEM_ATUALIZAR.id, novoDado);
+        const [resultado] = await database.listar(DEFULT_ITEM_ATUALIZAR.id)
+        deepEqual(resultado, expected);
     })
 
 })
