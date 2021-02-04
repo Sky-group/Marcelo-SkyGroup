8936a497ad191ae51b5bb3f507ea100b49df54ee
marcelocardosodev <cardosom2003dev@gmail.com>
2021-02-03 23:12:52 -0300

    Adicionando Swagger ao projeto

---
+++ 19-hapi-swagger/src/api.js
@@ -0,0 +1,54 @@
+// npm install hapi
+// npm i vision inert hapi-swagger
+
+const Hapi = require('hapi')
+const Context = require('./db/strategies/base/contextStrategy')
+const MongoDb = require('./db/strategies/mongodb/mongodb');
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema');
+const HeroRoutes = require('./routes/heroRoutes')
+
+const HapiSwagger = require('hapi-swagger');
+const Vision = require('vision')
+const Inert = require('inert')
+
+const app = new Hapi.Server({
+    port:5000
+
+});
+
+function mapRoutes (instance, methods) {
+    return methods.map(method => instance[method]())
+}
+
+async function main(){
+    const connection = MongoDb.connected();
+    const context = new Context(new MongoDb(connection,HeroiSchema));
+
+    const swaggerOption = {
+        info:{
+            title: 'API Herois - #CursoNodeBr',
+            version: '1.0'
+        },
+        lang: 'pt'
+    }
+    
+    await app.register([
+        Vision,
+        Inert ,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOption
+        }
+    ])
+    
+    app.route(
+        mapRoutes(new HeroRoutes(context), HeroRoutes.methods())
+    )
+
+    await app.start();
+    console.log("Servidor rodando na porta : ", app.info.port);
+
+    return app;
+}
+
+module.exports = main();
